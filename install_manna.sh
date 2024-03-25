f9dcc10ce1e5c983e29d0a4514e7fac55ef5ff54
Md Nuruzzaman Faruk <mdnfaruk.fin@gmail.com>
2024-03-25 11:31:02 +0200

    encrypting status check

--- install_manna.sh
+++ install_manna.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 MIND_SCRIPT_PATH="/home/pi/manna_mind_installation"
 CHECK_ENCRYPTION=$(lsblk -o type | grep crypt | wc -l)
 CONFIG_FILE="/etc/manna/device_config.json"
@@ -9,10 +8,14 @@ ENCRYPT_SCRIPT_PID=$(pidof -x "mk_encr_sd_rfs.sh")
 
 cd "$MIND_SCRIPT_PATH"
 
-if [[ -n $ENCRYPT_SERVICE_STATUS ]] || [[ -n $ENCRYPT_SCRIPT_PID ]]; then
-    echo "Encryption in progress, exiting"
-    exit 0
-fi
+check_encryption_progress() {
+    if [[ -n $ENCRYPT_SERVICE_STATUS ]] || [[ -n $ENCRYPT_SCRIPT_PID ]]; then
+        echo "Encryption in progress, exiting"
+        exit 0
+    fi
+}
+
+check_encryption_progress
 
 if [ $CHECK_ENCRYPTION -eq 1 ]; then
     echo "Encryption found"
@@ -20,7 +23,10 @@ else
     echo "Encryption not found, Insert at least 64GB empty USB stick for encryption"
     read -rp "Press 1 to start encryption " USER_INPUT
 
-    [[ "$USER_INPUT" == "1" ]] && sudo ./encrypt_image.sh || { echo "Invalid input. Exiting."; exit 1; }
+    [[ "$USER_INPUT" == "1" ]] && sudo ./encrypt_image.sh || {
+        echo "Invalid input. Exiting."
+        exit 1
+    }
 fi
 
 if [ -f $CONFIG_FILE ]; then
@@ -28,6 +34,7 @@ if [ -f $CONFIG_FILE ]; then
     read -rp "Press 3 for uninstall manna MIND " UNINSTALL_INPUT
     [[ "$UNINSTALL_INPUT" == "3" ]] && ./uninstall_manna_mind.sh
 else
+    check_encryption_progress
     echo "manna MIND is not installed"
 
     while ! curl -s --head https://www.google.com >/dev/null; do
