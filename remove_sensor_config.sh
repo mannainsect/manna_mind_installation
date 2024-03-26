9ccce1b5f5e0721d41e47ca461419cd97917a3e9
Md Nuruzzaman Faruk <mdnfaruk.fin@gmail.com>
2024-03-26 14:02:43 +0200

    added specific sensor deletion script

---
+++ remove_sensor_config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+SENSOR_CODE_LIST=$(jq -r '.SENSOR_CONFIGURATION[].SENSOR_CODE' "$CONFIG_FILE" | tr '\n' ',' | sed 's/,$//;s/,/, /g')
+
+
+echo -e "Which sensor config you want to remove?\n"
+echo -e "$SENSOR_CODE_LIST\n"
+
+while true; do
+    read -p "Insert sensor code name (exm: SCD30_1): " USER_INPUT
+    USER_INPUT=${USER_INPUT^^}
+    if [[ "${SENSOR_CODE_LIST,,}" == *"$USER_INPUT_LOWER"* ]]; then
+        break
+    else
+        echo "Invalid sensor code. Please try again."
+    fi
+done
+
+sudo sh -c "jq --arg input \"$USER_INPUT\" '.SENSOR_CONFIGURATION |= map(select(.SENSOR_CODE != \$input))' \"$CONFIG_FILE\" > output.json && mv output.json \"$CONFIG_FILE\"" && echo 'Deletion success'
